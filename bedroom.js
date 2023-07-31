25d71ac54be3fcab9cba4c3e0a420ff9b9ef9581
KavyaMittal123 <105212416+KavyaMittal123@users.noreply.github.com>
2023-07-31 20:52:28 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,48 @@
+img="";
+status="";
+object=[];
+
+function preload(){
+    img=loadImage("bedroom.jpg");
+}
+
+function setup(){
+    canvas=createCanvas(450,325);
+    canvas.center();
+    object_detector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status= Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+    status=true;
+}
+
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+        object=result;
+    }
+}
+
+function draw(){
+    image(img,0,0,450,325);
+    if(status!=""){
+        object_detector.detect(img,gotResult);
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        for(i=0;i<object.length;i++){
+            document.getElementById("status").innerHTML="Status= object detected";
+            fill(r,g,b);
+            percent=floor(object[i].confidence*100);
+            text(object[i].label+" "+percent+"%", object[i].x,object[i].y);
+            noFill();
+            stroke(r,g,b);
+            rect(object[i].x,object[i].y,object[i].width,object[i].height);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+
+function ac(){
+    window.location="ac.html";
+}
+
+function bottle(){
+    window.location="bottle.html";
+}
